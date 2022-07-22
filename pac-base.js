34c4a4771e86b2f84cf7ea50f77766bd2804e9f9
Alexander Vesyolkin <sashaasdzxc@gmail.com>
2022-07-22 18:45:43 +0300

    test

--- pac-base.js
+++ pac-base.js
@@ -1,11 +1,10 @@
 var ips = 'meduzaio'
 
 function FindProxyForURL(url, host) {
-    alert(host)
     host = host.replace('.', '')
 
     if (ips.indexOf(host) !== -1) {
-        alert('HELLO')
+        alert('MEDUZA!')
         return "HTTPS proxy-ssl.antizapret.prostovpn.org:3143; PROXY proxy-nossl.antizapret.prostovpn.org:29976; DIRECT";
     }
 
