24a15f3f639b221f3632d69afaf856028bc6159a
Alexander Vesyolkin <sashaasdzxc@gmail.com>
2022-07-22 19:02:24 +0300

    test my proxy

--- pac-base.js
+++ pac-base.js
@@ -4,8 +4,8 @@ function FindProxyForURL(url, host) {
     host = host.replace('.', '')
 
     if (ips.indexOf(host) !== -1) {
-        alert('MEDUZA!')
-        return "HTTP 89.22.231.161:80; DIRECT";
+        alert(host)
+        return "HTTP 89.22.231.161";
     }
 
     return  "DIRECT";
