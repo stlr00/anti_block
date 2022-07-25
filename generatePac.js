30f7cf1c9096ad0939c992175c854657b76005d1
Alexander Vesyolkin <sashaasdzxc@gmail.com>
2022-07-25 12:57:00 +0300

    proxy for safari instagram

--- generatePac.js
+++ generatePac.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const PROXIES = process.env.PROXY;
+const PROXY = process.env.PROXY;
 
 async function getList() {
     const res = await fetch('https://cloudflare-ipfs.com/ipfs/QmXPVA3P72hMURKvCmdQmdAVRk91J1WgEqsA7HPG3szH8Q/proxy-ssl.js')
@@ -10,8 +10,14 @@ async function getList() {
 async function writePac() {
     let pacFile = await getList()
 
-    // pacFile = pacFile.replace('HTTPS proxy-fbtw-ssl.antizapret.prostovpn.org:3143; DIRECT', PROXIES)
-    pacFile = pacFile.replace('HTTPS proxy-ssl.antizapret.prostovpn.org:3143; PROXY proxy-nossl.antizapret.prostovpn.org:29976; DIRECT', PROXIES)
+    pacFile = pacFile.replace(
+        'HTTPS proxy-fbtw-ssl.antizapret.prostovpn.org:3143; DIRECT',
+        'HTTPS proxy-ssl.antizapret.prostovpn.org:3143; HTTP proxy-nossl.antizapret.prostovpn.org:29976; DIRECT'
+    )
+    pacFile = pacFile.replace(
+        'HTTPS proxy-ssl.antizapret.prostovpn.org:3143; PROXY proxy-nossl.antizapret.prostovpn.org:29976; DIRECT',
+        PROXY
+    )
     fs.writeFileSync('./pac.js', pacFile)
 }
 
