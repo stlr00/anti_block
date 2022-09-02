59cd4ecc3f3b116e6129791fe26f1722c98c22ab
Alexander Vesyolkin <sashaasdzxc@gmail.com>
2022-09-02 11:42:24 +0300

    blocked ips

--- ddos.sh
+++ ddos.sh
@@ -4,7 +4,7 @@
 netstat -ntu | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | grep -v "127.0.0.1" > /usr/local/ddos/ddos.iplist
 
 # создаем DROP правила (не выдавать ответа сервера на запросы забаненых) для IP с количеством подключений более 50 и добавляем их в файл
-awk '{if ($1 > 50) {print "/sbin/iptables -A INPUT -s " $2 " -j DROP"; print "/sbin/iptables -A INPUT -d " $2 " -j DROP";}}' /usr/local/ddos/ddos.iplist >> /usr/local/ddos/iptables_ban.sh
+awk '{if ($1 > 50) {print "/usr/sbin/iptables -A INPUT -s " $2 " -j DROP"; print "/sbin/iptables -A INPUT -d " $2 " -j DROP";}}' /usr/local/ddos/ddos.iplist >> /usr/local/ddos/iptables_ban.sh
 
 # запускаем скрипт бана IP атакующих
 bash /usr/local/ddos/iptables_ban.sh
